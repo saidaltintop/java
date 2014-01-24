d721126f5e39a9ede141136fdd5b405e79b082a2
saltintop64 <s.altintop64@gmail.com>
2014-01-24 07:01:25 +0200

    Update Kullaniciadivesifrekontrol.java

--- Kullaniciadivesifrekontrol.java
+++ Kullaniciadivesifrekontrol.java
@@ -1,15 +1,14 @@
-
 public class Kullaniciadivesifrekontrol {
 	private String kullaniciadi="said";
 	private String sifre="1234";
-	Silgi sil =new Silgi();
+	Silgi sil =new Silgi();/*silgi class ını cagırdık*/
 	public boolean kullaniciadikontrol(String girilenkullaniciadi,String girilensifre)
 	{
 		if(girilenkullaniciadi.equalsIgnoreCase(kullaniciadi))//burda girilenkullaniciadi ile kullaniciadinin aynı olup olmadıgını kontrol eder ve aynıysa if içine girer
 			{/*saidaltıntop*/
 			if(girilensifre.equalsIgnoreCase(sifre))//burda girilensifre ile sifrenin aynı olup olmadıgını kontrol eder ve aynıysa if içine girer
 				{
-				sil.sil();
+				sil.sil();/*silgi class ının sil metotunu kullandık*/
 				return true;
 				}
 			else
