b975f239163e40a307c0a78d3839a35463a46ca7
saltintop64 <s.altintop64@gmail.com>
2014-01-24 06:14:38 +0200

    Create Silgi.java

---
+++ Silgi.java
@@ -0,0 +1,15 @@
+
+public class Silgi {
+
+	public Silgi()
+	{
+	sil();
+	}
+	public void sil()
+	{
+		for(int i=0;i<100;i++)
+		{
+		System.out.println();/*burda ekranda her satıra bir bosluk koyup bir alta geçiyor ve bunu 100 kere yapıyor*/
+		}
+	}
+}
