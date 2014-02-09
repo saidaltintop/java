6e1b355bc7b93c163b48795e6d83903bfd2e70bc
saltintop64 <s.altintop64@gmail.com>
2014-02-09 12:53:32 +0200

    sayitahminoyunu.java

---
+++ sayitahminoyunu.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class tahminoyunu {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args){	
+			Random rastgelesayi=new Random();//rastgele sayı atayabilmek için Random ı kullanacagız.
+			Scanner sayioku=new Scanner(System.in);//klavyeden girilen degeri okutabilmek için Scanner ı kullanacagız
+			int rastsayi=rastgelesayi.nextInt(200);//0 dan 200 e kadar rastgele bir sayi alır ve bunu rastsayi ya atar 
+			int i=0,sayi;//i yi kac defa sayiyi bulmak için deneme yapıldıgını bulmak için yazdık
+			
+			do
+			{
+				System.err.print("0 dan 200 e kadar bir sayi giriniz => ");//ekrana yazıyı yazdırmaya yarar.err kırmızı renkde yazıdırır(err hata mesajları ıcındır ama ben suan farklı bir renk olsun diye kullandım)
+			    sayi=sayioku.nextInt();//girdigimiz sayiyi sayi ya atar.
+				i++;
+				if(sayi<rastsayi)//eger random sayi bizim sayımızdan buyukse bu if içine girer
+				{
+					System.out.print(i+". denemeniz "+"\nSayiyi buyutunuz\n");
+				}//saidaltıntop
+				else if(sayi>rastsayi)//eger random sayi bizim sayimizdan kucukse bu else if içine girer
+				{
+					System.out.println(i+". denemeniz "+"\nSayiyi küçültunuz\n");
+				}
+				
+			}while(sayi!=rastsayi);//döngu random sayi bizim sayımıza eşit olasıya kadar devam eder
+			System.err.print("Tebrikler "+i+". denemede buldunuz..");//saidaltıntop
+	}
+}
