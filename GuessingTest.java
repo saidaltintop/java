8fa7a8b4ad9b6d524a4c7f3fc2682967794ab004
said altıntop <s.altintop64@gmail.com>
2014-03-20 04:02:58 +0200

    Create GuessingTest.java
    içinde kullandıgım Guessing sınıfı diger class lar içindedir.

---
+++ GuessingTest.java
@@ -0,0 +1,13 @@
+
+public class GuessingTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Guessing number=new Guessing();
+		System.err.println("Computer guessed "+number.calculate(100, 0)+" at "+number.getSteps()+" steps...");
+	}
+
+}
